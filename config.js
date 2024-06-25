a4f507fb34b25c1a3fffa788d279c1e9e68155e6
rustyda3 <rusty@trippy.host>
2024-06-25 19:18:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 module.exports = {
     token: process.env.token,
     prefix: ".",
-    allowedUserIDs: ['1149353212680339537', '#', '#']
+    allowedUserIDs: ['1149353212680339537', '791664496690069564', '#']
 };
